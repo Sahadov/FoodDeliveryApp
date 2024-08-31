869c9089c7035eef148dcc586bfa973f5b0920e4
Dmitry <mrsahadov@gmail.com>
2024-08-31 10:11:14 -0500

    add reusable bottomView for Auth screens

--- FoodDeliveryApp/FoodDeliveryApp/Resources/Colors.swift
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/Colors.swift
@@ -14,4 +14,5 @@ enum Colors {
     static let background = UIColor().hex(0xE5E5E5)
     static let white = UIColor.white
     static let yellow = UIColor().hex(0xF7B643)
+    static let gray4 = UIColor.systemGray4
 }
